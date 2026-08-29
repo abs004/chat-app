4e56492f78b6758adcb9b2c52ffad1505778155e
Abhishek <abhishek2004abs@gmail.com>
2026-08-29 15:20:44 +0530

    forgot button color change

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -206,7 +206,7 @@ export default function Login() {
                 </button>
               </div>
               <div className="flex justify-end mt-1.5">
-                <Link to="/forgot-password" className="text-gray-500 hover:text-gray-700">
+                <Link to="/forgot-password" className="text-xs font-medium text-gray-600 hover:text-gray-800">
                   Forgot password?
                 </Link>
               </div>
