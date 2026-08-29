9771ba6a7e48ff84df37efdb9d765559ebef4421
Abhishek <abhishek2004abs@gmail.com>
2026-08-29 21:36:25 +0530

    size adjustments

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -99,7 +99,7 @@ export default function Login() {
           {/* Brand */}
           <div className="relative z-10">
             <div className="flex items-center gap-2.5 mb-3">
-              <img src="/favicon-96x96.png" alt="G-Chat" className="w-8 h-8" />
+              <img src="/favicon-96x96.png" alt="G-Chat" className="w-12 h-12" />
               <div className="flex flex-col">
                 <span className="text-white font-semibold text-lg tracking-tight leading-tight" style={{fontFamily: "'Sora', sans-serif"}}>
                   G-Chat
